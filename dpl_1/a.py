c181f5daeeb873df360546a16e2cf75338fd96fc
sl-na <slink0051@gmail.com>
2026-09-02 16:02:11 +0900

    Improve coin change performance and simplify input parsing
    - Swap loops for better cache locality
    - Use list() for cleaner iterator conversion
    - Remove redundant conditional check

--- dpl_1/a.py
+++ dpl_1/a.py
@@ -7,17 +7,16 @@ from math import inf
 def min_coins(n, coins):
     dp = [inf] * (n + 1)
     dp[0] = 0
-    for i in range(1, n + 1):
-        for coin in coins:
-            if i >= coin:
-                dp[i] = min(dp[i], dp[i - coin] + 1)
+    for coin in coins:
+        for i in range(coin, n + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
     return dp[n]
 
 
 def main():
     it = map(int, sys.stdin.read().split())
     n, _ = next(it), next(it)
-    coins = [i for i in it]
+    coins = list(it)
     print(min_coins(n, coins))
 
 
